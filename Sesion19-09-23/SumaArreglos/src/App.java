6fd9eeea5d8e56ba0779bb7c8cfa9a614aa783de
HeylinPinedaSiu <142961091+HeylinPinedaSiu@users.noreply.github.com>
2023-11-11 17:03:28 -0500

    App.java

--- Sesion19-09-23/SumaArreglos/src/App.java
+++ Sesion19-09-23/SumaArreglos/src/App.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        System.out.print("Cantidad de notas:");
+        int cantidadNotas = lector.nextInt();
+
+        while (cantidadNotas <= 0) {
+            System.out.println("No puede ingresar valores menores o iguales a cero");
+            System.out.print("Cantidad de notas:");
+            cantidadNotas = lector.nextInt();
+        }
+
+        int[] notas = new int[cantidadNotas];
+        String[] nombresEstudiantes = new String[cantidadNotas];
+        int sumaNotas = 0;
+        System.out.println("Ingrese nombre y nota de cada estudiante del grupo: ");
+        
+        for (int i = 0; i < notas.length; i++) {
+            System.out.print("Estudiante[" + (i+1) + "] = ");
+            lector.nextLine();  //  \n
+            nombresEstudiantes[i] = lector.nextLine();
+            
+            System.out.print("Nota[" + (i+1) + "] = ");
+            notas[i] = lector.nextInt();
+            sumaNotas += notas[i];
+        }
+        
+        System.out.println(":::::::::::::::::::::::::::::::::::::");
+        System.out.println("Nombre completo\t\t\tNota");
+        for (int i = 0; i < nombresEstudiantes.length; i++) {
+            System.out.println(nombresEstudiantes[i] + "\t\t\t" + notas[i]);
+        }
+        
+        System.out.println(":::::::::::::::::::::::::::::::::::::");
+        System.out.println("El promedio de las notas es " + (double)sumaNotas/cantidadNotas);
+        lector.close();
+    }    
 }
